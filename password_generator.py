31f9f030e89d54c7002228ffea728b2dd76c0448
NAGALAPURAM HAREESWAR <135661045+Nagalapuramhareeswar@users.noreply.github.com>
2024-02-06 01:37:23 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -27,7 +27,7 @@ def main():
     passwordLengths = []
     print("Minimum length of password should be 3")
     for i in range(numPasswords):
-        length = int(input("Enter the length of Password #" + str(i+1) + " "))
+        length = int(input("Enter the length of Password #" + str(i+1) + " : "))
         if length<3:
             length = 3
         passwordLengths.append(length)
